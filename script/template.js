b70a7f8908edc06ad420c63990cf8ebe79cf6fce
FabianKlemenz <klemenz.f@outlook.com>
2026-09-22 15:43:29 +0200

    Move pokemon name above image and wrap image in its own colored div

--- script/template.js
+++ script/template.js
@@ -13,9 +13,11 @@ function getPokemonCardTemplate(pokemon) {
 
   return `
     <li>
-    <button data-id="card" class="pokemon-card ${mainType}" aria-label="Show details for ${pokemon.name}">
-    <img data-id="card-image" src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" />
+    <button data-id="card" class="pokemon-card" aria-label="Show details for ${pokemon.name}">
     <h3 class="pokemon-name">${pokemon.name}</h3>
+    <div class="pokemon-image-wrap ${mainType}">
+    <img data-id="card-image" src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" />
+    </div>
     <div class="type-badges">${typeBadgesHtml}</div>
     </button>
     </li>
