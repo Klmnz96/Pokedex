abc6f1b80c0a5ad17e2ab6d563bf9635c3b3f2d6
FabianKlemenz <klemenz.f@outlook.com>
2026-09-23 17:42:22 +0200

    Add getPokemonDialogTemplate function

--- script/template.js
+++ script/template.js
@@ -23,3 +23,13 @@ function getPokemonCardTemplate(pokemon) {
     </li>
     `;
 }
+
+function getPokemonDialogTemplate(pokemon) {
+  return `
+  <div data-id="overlay-pokemon-name">
+  <button data-id="close-dialog-button">Close</button>
+  <h2 class="pokemon-name">${pokemon.name}</h2>
+  <img data-id="dialog-image" src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" />
+  </div>
+  `;
+}
